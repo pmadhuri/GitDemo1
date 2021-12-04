8150a5b6b88221ce9a7e75f266876fcfe7287629
Madhuri <guravmadhuri93@gmail.com>
2021-12-04 23:44:26 +0530

    first commit

---
+++ src/test/java/qaclickacademy/MavenJava/APITest.java
@@ -0,0 +1,17 @@
+package qaclickacademy.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class APITest {
+@Test
+public void postjira() {
+	
+	System.out.println("postjerA");
+}
+
+@Test
+public void deletTwitter() {
+	System.out.println("DELETEtWITTER");
+}
+}
+
---
+++ src/test/java/qaclickacademy/MavenJava/AppiumTest.java
@@ -0,0 +1,14 @@
+package qaclickacademy.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+@Test
+public void NativeAppAndroid() {
+	System.out.println("Native AppAndroid");
+}
+@Test
+public void IOSApp() {
+	System.out.println("iosApp");
+}
+}
